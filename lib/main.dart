d48ca3aade83ee847cc46774bc4f7aa7089334ff
Julian Starchenko <jstar@jinr.ru>
2019-08-06 18:30:54 +0300

    In the process

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,49 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> score = [];
+  List<String> questions = [
+    'First',
+    'Second',
+    'Third',
+  ];
+  List<bool> answers = [true, false, false];
+  int index = 0;
+  Icon correct = Icon(Icons.check, color: Colors.green);
+  Icon wrong = Icon(Icons.close, color: Colors.red);
+  int ButtonId = 0;
+  Icon CheckAnswer() {
+    print('ButtonID: $ButtonId, index: $index');
+    return (ButtonId == 1 && answers[index % 3] ||
+            ButtonId == 2 && !answers[index % 3]
+        ? correct
+        : wrong);
+  }
+
+  List<Icon> getLast(int last) {
+    List<Icon> res = [];
+    while (score.length < last && last >= 0) {
+      --last;
+    }
+    int ln = score.length;
+    print('Score Size: $ln, Last: $last');
+    for (int i = last; i >= 0; --i) {
+      res.add(score[score.length - 1 - i]);
+    }
+    return res;
+  }
+
+  void ButtonPressed(int id) {
+    setState(() {
+      ButtonId = id;
+      score.add(CheckAnswer());
+      ++index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<Icon> scores = getLast(3);
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -37,7 +78,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[index % 3],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -60,9 +101,7 @@ class _QuizPageState extends State<QuizPage> {
                   fontSize: 20.0,
                 ),
               ),
-              onPressed: () {
-                //The user picked true.
-              },
+              onPressed: () => ButtonPressed(1),
             ),
           ),
         ),
@@ -78,13 +117,15 @@ class _QuizPageState extends State<QuizPage> {
                   color: Colors.white,
                 ),
               ),
-              onPressed: () {
-                //The user picked false.
-              },
+              onPressed: () => ButtonPressed(2),
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Expanded(
+          child: Row(
+            children: scores,
+          ),
+        ),
       ],
     );
   }
